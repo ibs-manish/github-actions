c84f44b232db4f9df27662b5560d40e9227d3733
manishkumar201 <manishkumar201@impressico.com>
2024-08-02 15:25:14 +0530

    test: testing ssm command to ec2

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ aws_region = os.getenv('AWS_REGION') if(os.getenv('AWS_REGION')) else None
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID') if ( os.getenv('AWS_ACCESS_KEY_ID') ) else None
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY') if(os.getenv('AWS_SECRET_ACCESS_KEY')) else None
 instance_id = os.getenv('INSTANCE_ID') if(os.getenv('INSTANCE_ID')) else None
+working_dir = os.getenv('WORKING_DIR') if(os.getenv('WORKING_DIR')) else None
+commands = os.getenv('COMMANDS') if(os.getenv('COMMANDS')) else None
 
 #making checks for essential variables
 assert job,                   "Must Provide job_name for this workflow"
@@ -17,7 +19,7 @@ assert aws_secret_access_key, "Must Provide aws_secret_access_key for this workf
 assert instance_id,           "Must Provide instance_id for this workflow"
 
 #initiating boto3
-# SSM = boto3.client('ssm', region_name=aws_region)
+SSM = boto3.client('ssm', region_name=aws_region)
 
 def main():
     # running jobs
@@ -29,10 +31,24 @@ def main():
 
 def aws_ssm_send_command():
     print("---------------running aws_ssm_send_command---------------")
+    response = SSM.send_command(
+        InstanceIds=[ instance_id ],
+        DocumentName='AWS-RunShellScript',
+        Parameters={
+            'workingDirectory': [ working_dir ],
+            'commands': [ commands ]
 
-    response = boto3.client('ec2',region_name=aws_region).describe_instances(
-        MaxResults=123
+        }
     )
+    # response = SSM.send_command(
+    #     InstanceIds=[ instance_id ],
+    #     DocumentName='AWS-RunShellScript',
+    #     Parameters={
+    #         'workingDirectory': [ working_dir ],
+    #         'commands': [ commands ]
+
+    #     }
+    # )
     print(response)
 
 if __name__ == "__main__":
