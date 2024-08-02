06eece787219c3eaffe50bfc41d15d12190d79b8
manishkumar201 <manishkumar201@impressico.com>
2024-08-02 13:57:52 +0530

    fix: fixed mistake in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import boto3
 max_number_tries = 10
 job = os.getenv("JOB") if(os.getenv("JOB")) else None
 aws_region = os.getenv('AWS_REGION') if(os.getenv('AWS_REGION')) else None
-aws_access_key_id = int(os.getenv('AWS_ACCESS_KEY_ID')) if ( os.getenv('AWS_ACCESS_KEY_ID') ) else None
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID') if ( os.getenv('AWS_ACCESS_KEY_ID') ) else None
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY') if(os.getenv('AWS_SECRET_ACCESS_KEY')) else None
 instance_id = os.getenv('INSTANCE_ID') if(os.getenv('INSTANCE_ID')) else None
 
